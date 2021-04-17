2408b3da851db440324afc44bb3939e6bc158a8b
lijiupeng <202062241@qq.com>
2021-04-17 18:21:46 +0800

    完善了用户管理 新增的字典关联 都未玩不完善好

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+$END$
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login",
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
