2194e68cf0e3009db95007828e1d3f4fda283f33
lijiupeng <202062241@qq.com>
2021-04-19 10:47:52 +0800

    代码恢复了一点

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,130 @@
 <template>
-$END$
+  <div class="login-box">
+    <div>
+      <div class="login-logo">
+        <i class="el-icon-office-building"></i>
+        <span class="title">基础平台</span>
+      </div>
+      <el-form :model="formData" :rules="rules" ref="refForm">
+        <el-form-item prop="username">
+          <el-input
+            v-model="formData.username"
+            size="large"
+            placeholder="admin"
+            prefix-icon="el-icon-user"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formData.password"
+            size="large"
+            type="password"
+            placeholder="123456"
+            prefix-icon="el-icon-lock"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            :loading="submitLoading"
+            @click="handleSubmit()"
+          >
+            登录
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  onMounted,
+  // getCurrentInstance,
+} from "vue";
 
 export default defineComponent({
   name: "Login",
   setup() {
-    return {}
+    // const { ctx } = getCurrentInstance();
+
+    const formControl = reactive({
+      submitLoading: false,
+      formData: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          { required: true, message: "账号不能为空", trigger: "change" },
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "change" },
+        ],
+      },
+      handleSubmit() {
+        this.$refs["refForm"].validate((valid) => {
+          if (!valid) return;
+          this.submitLoading = true;
+          this.$Api
+            .Post("login", {
+              username: this.formData.username,
+              password: this.formData.password,
+            })
+            .then((response) => {
+              this.$Api.AddHeaders({ token: response.data });
+              localStorage.setItem("token", response.data);
+              this.$router.push("/user");
+            })
+            .catch(() => {
+              this.submitLoading = false;
+            });
+        });
+      },
+    });
+    onMounted(() => {});
+    return {
+      ...toRefs(formControl),
+    };
   },
-})
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.login-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: url("~@/assets/images/login.svg");
+  background-size: 100% 100%;
 
+  .login-logo {
+    margin-bottom: 30px;
+    height: 50px;
+
+    i {
+      font-size: 40px;
+      margin-right: 10px;
+    }
+
+    .title {
+      font-size: 38px;
+    }
+  }
+
+  .el-form {
+    width: 350px;
+
+    .el-button {
+      width: 100%;
+    }
+  }
+}
 </style>
--- src/views/user/Index.vue
+++ src/views/user/Index.vue
@@ -3,54 +3,186 @@
     <el-header height="135px">
       <div class="filters">
         <el-form :inline="true" :model="filterForm" class="form-filter">
-          <el-form-item label="审批人">
-            <el-input v-model="filterForm.user" placeholder="审批人"></el-input>
+          <el-form-item label="昵称">
+            <el-input v-model="filterForm.nickname"></el-input>
           </el-form-item>
-          <el-form-item label="活动区域">
-            <el-select v-model="filterForm.region" placeholder="活动区域">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
+          <el-form-item label="账号">
+            <el-input v-model="filterForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="filterForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号">
+            <el-input v-model="filterForm.phone"></el-input>
           </el-form-item>
         </el-form>
         <div class="group-btn">
           <el-button-group>
-            <el-button type="primary">添加</el-button>
+            <el-button type="primary" @click="dialogFormVisible = true"
+              >添加</el-button
+            >
             <el-button type="primary">删除</el-button>
           </el-button-group>
         </div>
       </div>
     </el-header>
     <div class="table-main">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180">
+      <el-table :data="tableData" v-loading="tableLoading" style="width: 100%">
+        <el-table-column prop="id" label="ID" width="100"> </el-table-column>
+        <el-table-column prop="nickname" label="昵称" width="180">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
+        <el-table-column prop="username" label="账号" width="180">
         </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
+        <el-table-column prop="email" label="邮箱"> </el-table-column>
+        <el-table-column prop="phone" label="手机号"> </el-table-column>
+        <el-table-column prop="remakes" label="备注"> </el-table-column>
       </el-table>
+
+      <el-pagination
+        @size-change="refreshTableData()"
+        @current-change="refreshTableData()"
+        v-model:current-page="pageInfo.current"
+        :page-sizes="[10, 20, 30, 50]"
+        v-model:page-size="pageInfo.size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pageInfo.total"
+      >
+      </el-pagination>
     </div>
+
+    <el-dialog title="添加用户" v-model="dialogFormVisible">
+      <el-form
+        :model="formData"
+        :rules="formRules"
+        ref="form"
+        label-width="100px"
+      >
+        <el-form-item label="昵称" prop="nickname">
+          <el-input v-model="formData.nickname" maxlength="26"></el-input>
+        </el-form-item>
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="formData.username" maxlength="20"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="formData.password" maxlength="20"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="formData.email" maxlength="255"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="phone">
+          <el-input v-model="formData.phone" maxlength="20"></el-input>
+        </el-form-item>
+        <el-form-item label="备注" prop="remakes">
+          <el-input
+            v-model="formData.remakes"
+            type="textarea"
+            :rows="4"
+            maxlength="255"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" :loading="submitLoading" @click="submit()"
+            >确 定</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
   </el-container>
 </template>
 
 <script>
-import { defineComponent, toRefs, reactive } from "vue";
+import { defineComponent, toRefs, reactive, getCurrentInstance } from "vue";
 
 export default defineComponent({
   name: "user",
   setup() {
-    const formControl = reactive({
-      filterForm: {},
+    const { ctx } = getCurrentInstance();
+
+    const filterFormControl = reactive({
+      filterForm: {
+        nickname: "",
+        username: "",
+        email: "",
+        phone: "",
+      },
     });
     const tableControl = reactive({
+      tableLoading: true,
       tableData: [],
+      pageInfo: {
+        current: 1,
+        size: 10,
+        total: 0,
+      },
+      refreshTableData() {
+        this.tableLoading = true;
+        const params = {};
+        params["pageInfo.size"] = this.pageInfo.size;
+        params["pageInfo.current"] = this.pageInfo.current;
+        ctx.$Api
+          .Get("system/sys-user", params)
+          .then((response) => {
+            this.tableData = response.data.records;
+            this.pageInfo.total = response.data.total;
+            this.tableLoading = false;
+          })
+          .catch(() => {
+            this.tableLoading = false;
+          });
+      },
     });
-    for (let i = 0; i < 40; i++) {
-      tableControl.tableData.push({ name: i });
-    }
+    tableControl.refreshTableData();
+
+    const formControl = reactive({
+      dialogFormVisible: false,
+      formRules: {
+        nickname: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+        username: [{ required: true, message: "请输入账号", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        email: [
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"],
+          },
+        ],
+      },
+      formData: {
+        nickname: "",
+        username: "",
+        password: "",
+        email: "",
+        phone: "",
+        remakes: "",
+      },
+      submitLoading: false,
+      submit() {
+        this.$refs["form"].validate((valid) => {
+          if (!valid) return;
+          this.submitLoading = true;
+          this.$Api
+            .Post("system/sys-user", this.formData)
+            .then(() => {
+              this.submitLoading = false;
+              this.dialogFormVisible = false;
+              this.refreshTableData();
+              this.$message.success("提交成功");
+            })
+            .catch(() => {
+              this.submitLoading = false;
+            });
+        });
+      },
+    });
+
     return {
-      ...toRefs(formControl),
+      ...toRefs(filterFormControl),
       ...toRefs(tableControl),
+      ...toRefs(formControl),
     };
   },
 });
@@ -58,8 +190,6 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .el-container {
-  //background-color: #f2f7f8;
-  background-image: linear-gradient(transparent -10px, #f2f7f8);
   .el-header {
     .filters {
       position: relative;
@@ -87,6 +217,9 @@ export default defineComponent({
     background: #fff;
     //box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.1);
     //border-radius: 10px;
+    .el-pagination {
+      margin: 20px 0;
+    }
   }
 }
 </style>
